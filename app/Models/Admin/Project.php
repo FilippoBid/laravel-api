6798060f1fd55d10ddea1d3a875815750f515672
FilippoBid <FilippoPerrone1997@gmail.com>
2023-02-15 16:22:37 +0100

    init

---
+++ app/Models/Admin/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $fillable=[
+        "name","description","cover_img","github_link"
+        
+    ];
+    
+    use HasFactory;
+
+    public function types(){
+        return $this->belongsTo(Type::class);
+    }
+
+    public function technologies() {
+        return $this->belongsToMany(Technology::class);
+    }
+    
+}
